c0751aa10450e240fc0cd75b937c7f53865f5eb7
Markus Worchel <mworchel@hotmail.de>
2020-06-22 11:26:14 +0200

    Fix for multiprocessing usage

--- generate.py
+++ generate.py
@@ -15,188 +15,188 @@ from PIL import Image
 from scipy.spatial.transform import Rotation as R
 import trimesh
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Extract meshes from occupancy process.'
+    )
+    parser.add_argument('config', type=str, help='Path to config file.')
+    parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
+    parser.add_argument('--upsampling-steps', type=int, default=-1,
+                        help='Overrites the default upsampling steps in config')
+    parser.add_argument('--refinement-step', type=int, default=-1,
+                        help='Overrites the default refinement steps in config')
 
-parser = argparse.ArgumentParser(
-    description='Extract meshes from occupancy process.'
-)
-parser.add_argument('config', type=str, help='Path to config file.')
-parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
-parser.add_argument('--upsampling-steps', type=int, default=-1,
-                    help='Overrites the default upsampling steps in config')
-parser.add_argument('--refinement-step', type=int, default=-1,
-                    help='Overrites the default refinement steps in config')
 
+    args = parser.parse_args()
+    cfg = config.load_config(args.config, 'configs/default.yaml')
+    is_cuda = (torch.cuda.is_available() and not args.no_cuda)
+    device = torch.device("cuda" if is_cuda else "cpu")
 
-args = parser.parse_args()
-cfg = config.load_config(args.config, 'configs/default.yaml')
-is_cuda = (torch.cuda.is_available() and not args.no_cuda)
-device = torch.device("cuda" if is_cuda else "cpu")
+    # Overwrite upsamping and refinement step if desired
+    if args.upsampling_steps != -1:
+        cfg['generation']['upsampling_steps'] = args.upsampling_steps
+    if args.refinement_step != -1:
+        cfg['generation']['refinement_step'] = args.refinement_step
 
-# Overwrite upsamping and refinement step if desired
-if args.upsampling_steps != -1:
-    cfg['generation']['upsampling_steps'] = args.upsampling_steps
-if args.refinement_step != -1:
-    cfg['generation']['refinement_step'] = args.refinement_step
+    # Shortcuts
+    out_dir = cfg['training']['out_dir']
+    generation_dir = os.path.join(out_dir, cfg['generation']['generation_dir'])
+    out_time_file = os.path.join(generation_dir, 'time_generation_full.pkl')
+    out_time_file_class = os.path.join(generation_dir, 'time_generation.pkl')
 
-# Shortcuts
-out_dir = cfg['training']['out_dir']
-generation_dir = os.path.join(out_dir, cfg['generation']['generation_dir'])
-out_time_file = os.path.join(generation_dir, 'time_generation_full.pkl')
-out_time_file_class = os.path.join(generation_dir, 'time_generation.pkl')
+    batch_size = cfg['generation']['batch_size']
+    input_type = cfg['data']['input_type']
+    vis_n_outputs = cfg['generation']['vis_n_outputs']
+    mesh_extension = cfg['generation']['mesh_extension']
 
-batch_size = cfg['generation']['batch_size']
-input_type = cfg['data']['input_type']
-vis_n_outputs = cfg['generation']['vis_n_outputs']
-mesh_extension = cfg['generation']['mesh_extension']
+    # Dataset
+    # This is for DTU when we parallelise over images
+    # we do not want to treat different images from same object as
+    # different objects
+    cfg['data']['split_model_for_images'] = False
+    dataset = config.get_dataset(cfg, mode='test', return_idx=True)
 
-# Dataset
-# This is for DTU when we parallelise over images
-# we do not want to treat different images from same object as
-# different objects
-cfg['data']['split_model_for_images'] = False
-dataset = config.get_dataset(cfg, mode='test', return_idx=True)
+    # Model
+    model = config.get_model(cfg, device=device, len_dataset=len(dataset))
 
-# Model
-model = config.get_model(cfg, device=device, len_dataset=len(dataset))
+    checkpoint_io = CheckpointIO(out_dir, model=model)
+    checkpoint_io.load(cfg['test']['model_file'])
 
-checkpoint_io = CheckpointIO(out_dir, model=model)
-checkpoint_io.load(cfg['test']['model_file'])
+    # Generator
+    generator = config.get_generator(model, cfg, device=device)
 
-# Generator
-generator = config.get_generator(model, cfg, device=device)
+    torch.manual_seed(0)
+    # Loader
+    test_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, num_workers=0, shuffle=True)
 
-torch.manual_seed(0)
-# Loader
-test_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=1, num_workers=0, shuffle=True)
+    # Statistics
+    time_dicts = []
+    vis_file_dict = {}
 
-# Statistics
-time_dicts = []
-vis_file_dict = {}
+    # Generate
+    model.eval()
 
-# Generate
-model.eval()
+    # Count how many models already created
+    model_counter = defaultdict(int)
+    for it, data in enumerate(tqdm(test_loader)):
+        # Output folders
+        mesh_dir = os.path.join(generation_dir, 'meshes')
+        in_dir = os.path.join(generation_dir, 'input')
+        generation_vis_dir = os.path.join(generation_dir, 'vis', )
+        generation_paper_vis_dir = os.path.join(
+            generation_dir, 'visualizations_paper')
 
-# Count how many models already created
-model_counter = defaultdict(int)
-for it, data in enumerate(tqdm(test_loader)):
-    # Output folders
-    mesh_dir = os.path.join(generation_dir, 'meshes')
-    in_dir = os.path.join(generation_dir, 'input')
-    generation_vis_dir = os.path.join(generation_dir, 'vis', )
-    generation_paper_vis_dir = os.path.join(
-        generation_dir, 'visualizations_paper')
-
-    # Get index etc.
-    idx = data['idx'].item()
-    try:
-        model_dict = dataset.get_model_dict(idx)
-    except AttributeError:
-        model_dict = {'model': str(idx), 'category': 'na', 'category_id': 0000}
-
-    modelname = model_dict['model']
-    category = model_dict.get('category', 'na')
-    category_id = model_dict.get('category_id', 0000)
-    mesh_dir = os.path.join(mesh_dir, category)
-    in_dir = os.path.join(in_dir, category)
-    generation_vis_dir = os.path.join(generation_vis_dir, category)
-    generation_paper_vis_dir = os.path.join(generation_paper_vis_dir, category)
-
-    if not os.path.exists(mesh_dir):
-        os.makedirs(mesh_dir)
-    if not os.path.exists(generation_vis_dir) and vis_n_outputs > 0:
-        os.makedirs(generation_vis_dir)
-
-    # Timing dict
-    time_dict = {
-        'idx': idx,
-        'class_name': category,
-        'class_id': category_id,
-        'modelname': modelname,
-    }
-    time_dicts.append(time_dict)
-
-    # Generate outputs
-    out_file_dict = {}
-
-    # add empty list to vis_out_file for this category
-    if category not in vis_file_dict.keys():
-        vis_file_dict[category] = []
-
-    try:
-        t0 = time.time()
-        out = generator.generate_mesh(data)
-        time_dict['mesh'] = time.time() - t0
-        # Get statistics
+        # Get index etc.
+        idx = data['idx'].item()
         try:
-            mesh, stats_dict = out
-        except TypeError:
-            mesh, stats_dict = out, {}
-        time_dict.update(stats_dict)
+            model_dict = dataset.get_model_dict(idx)
+        except AttributeError:
+            model_dict = {'model': str(idx), 'category': 'na', 'category_id': 0000}
 
-        # Write output
-        mesh_out_file = os.path.join(
-            mesh_dir, '%s.%s' % (modelname, mesh_extension))
-        mesh.export(mesh_out_file)
-        out_file_dict['mesh'] = mesh_out_file
+        modelname = model_dict['model']
+        category = model_dict.get('category', 'na')
+        category_id = model_dict.get('category_id', 0000)
+        mesh_dir = os.path.join(mesh_dir, category)
+        in_dir = os.path.join(in_dir, category)
+        generation_vis_dir = os.path.join(generation_vis_dir, category)
+        generation_paper_vis_dir = os.path.join(generation_paper_vis_dir, category)
 
-        # For DTU save also transformed-back mesh to file
-        if cfg['data']['dataset_name'] == 'DTU':
-            scale_mat = data.get('camera.scale_mat_0')[0]
-            mesh_transformed = transform_mesh(mesh, scale_mat)
+        if not os.path.exists(mesh_dir):
+            os.makedirs(mesh_dir)
+        if not os.path.exists(generation_vis_dir) and vis_n_outputs > 0:
+            os.makedirs(generation_vis_dir)
+
+        # Timing dict
+        time_dict = {
+            'idx': idx,
+            'class_name': category,
+            'class_id': category_id,
+            'modelname': modelname,
+        }
+        time_dicts.append(time_dict)
+
+        # Generate outputs
+        out_file_dict = {}
+
+        # add empty list to vis_out_file for this category
+        if category not in vis_file_dict.keys():
+            vis_file_dict[category] = []
+
+        try:
+            t0 = time.time()
+            out = generator.generate_mesh(data)
+            time_dict['mesh'] = time.time() - t0
+            # Get statistics
+            try:
+                mesh, stats_dict = out
+            except TypeError:
+                mesh, stats_dict = out, {}
+            time_dict.update(stats_dict)
+
+            # Write output
             mesh_out_file = os.path.join(
-                mesh_dir, '%s_world_scale.%s' % (modelname, mesh_extension))
-            mesh_transformed.export(mesh_out_file)
+                mesh_dir, '%s.%s' % (modelname, mesh_extension))
+            mesh.export(mesh_out_file)
+            out_file_dict['mesh'] = mesh_out_file
 
-    except RuntimeError:
-        print("Error generating mesh %s (%s)." % (modelname, category))
-
-    # Copy to visualization directory for first vis_n_output samples
-    c_it = model_counter[category]
-    if c_it < vis_n_outputs:
-        # add model to vis_out_file
-        vis_file_dict[str(category)].append(modelname)
-        # Save output files
-        for k, filepath in out_file_dict.items():
-            ext = os.path.splitext(filepath)[1]
-            out_file = os.path.join(generation_vis_dir, '%02d_%s%s'
-                                    % (c_it, k, ext))
+            # For DTU save also transformed-back mesh to file
             if cfg['data']['dataset_name'] == 'DTU':
-                # rotate for DTU to visualization purposes
-                r = R.from_euler('xz', [-90, 10], degrees=True).as_matrix() @ \
-                    R.from_euler('xzy', [220, 44.9, 10.6], degrees=True
-                                 ).as_matrix()
-                transform = np.eye(4).astype(np.float32)
-                transform[:3, :3] = r.astype(np.float32)
-                mesh = transform_mesh(mesh, transform)
-                mesh.export(out_file)
-            else:
-                shutil.copyfile(filepath, out_file)
+                scale_mat = data.get('camera.scale_mat_0')[0]
+                mesh_transformed = transform_mesh(mesh, scale_mat)
+                mesh_out_file = os.path.join(
+                    mesh_dir, '%s_world_scale.%s' % (modelname, mesh_extension))
+                mesh_transformed.export(mesh_out_file)
 
-        if cfg['data']['input_type'] == 'image':
-            img = data.get('inputs')[0].permute(1, 2, 0).numpy()
-            img = (img * 255).astype(np.uint8)
-            img = Image.fromarray(img)
-            out_file = os.path.join(generation_vis_dir, '%02d_input.jpg'
-                                    % (c_it))
-            img.save(out_file)
+        except RuntimeError:
+            print("Error generating mesh %s (%s)." % (modelname, category))
 
-    model_counter[category] += 1
+        # Copy to visualization directory for first vis_n_output samples
+        c_it = model_counter[category]
+        if c_it < vis_n_outputs:
+            # add model to vis_out_file
+            vis_file_dict[str(category)].append(modelname)
+            # Save output files
+            for k, filepath in out_file_dict.items():
+                ext = os.path.splitext(filepath)[1]
+                out_file = os.path.join(generation_vis_dir, '%02d_%s%s'
+                                        % (c_it, k, ext))
+                if cfg['data']['dataset_name'] == 'DTU':
+                    # rotate for DTU to visualization purposes
+                    r = R.from_euler('xz', [-90, 10], degrees=True).as_matrix() @ \
+                        R.from_euler('xzy', [220, 44.9, 10.6], degrees=True
+                                    ).as_matrix()
+                    transform = np.eye(4).astype(np.float32)
+                    transform[:3, :3] = r.astype(np.float32)
+                    mesh = transform_mesh(mesh, transform)
+                    mesh.export(out_file)
+                else:
+                    shutil.copyfile(filepath, out_file)
 
-# Create pandas dataframe and save
-time_df = pd.DataFrame(time_dicts)
-time_df.set_index(['idx'], inplace=True)
-time_df.to_pickle(out_time_file)
+            if cfg['data']['input_type'] == 'image':
+                img = data.get('inputs')[0].permute(1, 2, 0).numpy()
+                img = (img * 255).astype(np.uint8)
+                img = Image.fromarray(img)
+                out_file = os.path.join(generation_vis_dir, '%02d_input.jpg'
+                                        % (c_it))
+                img.save(out_file)
 
-# Create pickle files  with main statistics
-time_df_class = time_df.groupby(by=['class_name']).mean()
-time_df_class.to_pickle(out_time_file_class)
+        model_counter[category] += 1
 
-# Print results
-time_df_class.loc['mean'] = time_df_class.mean()
-print('Timings [s]:')
-print(time_df_class)
+    # Create pandas dataframe and save
+    time_df = pd.DataFrame(time_dicts)
+    time_df.set_index(['idx'], inplace=True)
+    time_df.to_pickle(out_time_file)
 
-# save vis_out_file
-vis_file_dict_name = os.path.join(generation_dir, 'vis', 'visualization_files')
-np.save(vis_file_dict_name, vis_file_dict)
+    # Create pickle files  with main statistics
+    time_df_class = time_df.groupby(by=['class_name']).mean()
+    time_df_class.to_pickle(out_time_file_class)
+
+    # Print results
+    time_df_class.loc['mean'] = time_df_class.mean()
+    print('Timings [s]:')
+    print(time_df_class)
+
+    # save vis_out_file
+    vis_file_dict_name = os.path.join(generation_dir, 'vis', 'visualization_files')
+    np.save(vis_file_dict_name, vis_file_dict)
--- train.py
+++ train.py
@@ -9,162 +9,163 @@ from im2mesh import config, data
 from im2mesh.checkpoints import CheckpointIO
 import logging
 
-logger_py = logging.getLogger(__name__)
+if __name__ == '__main__':
+    logger_py = logging.getLogger(__name__)
 
 
-# Arguments
-parser = argparse.ArgumentParser(
-    description='Train implicit representations without 3D supervision.'
-)
-parser.add_argument('config', type=str, help='Path to config file.')
-parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
-parser.add_argument('--exit-after', type=int, default=-1,
-                    help='Checkpoint and exit after specified number of '
-                         'seconds with exit code 2.')
+    # Arguments
+    parser = argparse.ArgumentParser(
+        description='Train implicit representations without 3D supervision.'
+    )
+    parser.add_argument('config', type=str, help='Path to config file.')
+    parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
+    parser.add_argument('--exit-after', type=int, default=-1,
+                        help='Checkpoint and exit after specified number of '
+                            'seconds with exit code 2.')
 
-args = parser.parse_args()
-cfg = config.load_config(args.config, 'configs/default.yaml')
-is_cuda = (torch.cuda.is_available() and not args.no_cuda)
-device = torch.device("cuda" if is_cuda else "cpu")
+    args = parser.parse_args()
+    cfg = config.load_config(args.config, 'configs/default.yaml')
+    is_cuda = (torch.cuda.is_available() and not args.no_cuda)
+    device = torch.device("cuda" if is_cuda else "cpu")
 
-# Shorthands
-out_dir = cfg['training']['out_dir']
-backup_every = cfg['training']['backup_every']
-exit_after = args.exit_after
-lr = cfg['training']['learning_rate']
-batch_size = cfg['training']['batch_size']
-batch_size_val = cfg['training']['batch_size_val']
-n_workers = cfg['training']['n_workers']
-t0 = time.time()
+    # Shorthands
+    out_dir = cfg['training']['out_dir']
+    backup_every = cfg['training']['backup_every']
+    exit_after = args.exit_after
+    lr = cfg['training']['learning_rate']
+    batch_size = cfg['training']['batch_size']
+    batch_size_val = cfg['training']['batch_size_val']
+    n_workers = cfg['training']['n_workers']
+    t0 = time.time()
 
-# Set mesh extraction to low resolution for fast visuliation
-# during training
-cfg['generation']['upsampling_steps'] = 2
-cfg['generation']['refinement_step'] = 0
+    # Set mesh extraction to low resolution for fast visuliation
+    # during training
+    cfg['generation']['upsampling_steps'] = 2
+    cfg['generation']['refinement_step'] = 0
 
-model_selection_metric = cfg['training']['model_selection_metric']
-if cfg['training']['model_selection_mode'] == 'maximize':
-    model_selection_sign = 1
-elif cfg['training']['model_selection_mode'] == 'minimize':
-    model_selection_sign = -1
-else:
-    raise ValueError('model_selection_mode must be '
-                     'either maximize or minimize.')
+    model_selection_metric = cfg['training']['model_selection_metric']
+    if cfg['training']['model_selection_mode'] == 'maximize':
+        model_selection_sign = 1
+    elif cfg['training']['model_selection_mode'] == 'minimize':
+        model_selection_sign = -1
+    else:
+        raise ValueError('model_selection_mode must be '
+                        'either maximize or minimize.')
 
-# Output directory
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+    # Output directory
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-train_dataset = config.get_dataset(cfg, mode='train')
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=batch_size, num_workers=n_workers, shuffle=True,
-    collate_fn=data.collate_remove_none,
-)
-val_dataset = config.get_dataset(cfg, mode='val')
-val_loader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=batch_size_val, num_workers=int(n_workers/2),
-    shuffle=True, collate_fn=data.collate_remove_none,
-)
-data_viz = next(iter(val_loader))
-model = config.get_model(cfg, device=device, len_dataset=len(train_dataset))
+    train_dataset = config.get_dataset(cfg, mode='train')
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, num_workers=n_workers, shuffle=True,
+        collate_fn=data.collate_remove_none,
+    )
+    val_dataset = config.get_dataset(cfg, mode='val')
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size_val, num_workers=int(n_workers/2),
+        shuffle=True, collate_fn=data.collate_remove_none,
+    )
+    data_viz = next(iter(val_loader))
+    model = config.get_model(cfg, device=device, len_dataset=len(train_dataset))
 
-# Initialize training
-optimizer = optim.Adam(model.parameters(), lr=lr)
+    # Initialize training
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
-generator = config.get_generator(model, cfg, device=device)
+    generator = config.get_generator(model, cfg, device=device)
 
-trainer = config.get_trainer(model, optimizer, cfg, device=device,
-                             generator=generator)
-checkpoint_io = CheckpointIO(out_dir, model=model, optimizer=optimizer)
-try:
-    load_dict = checkpoint_io.load('model.pt')
-except FileExistsError:
-    load_dict = dict()
+    trainer = config.get_trainer(model, optimizer, cfg, device=device,
+                                generator=generator)
+    checkpoint_io = CheckpointIO(out_dir, model=model, optimizer=optimizer)
+    try:
+        load_dict = checkpoint_io.load('model.pt')
+    except FileExistsError:
+        load_dict = dict()
 
-epoch_it = load_dict.get('epoch_it', -1)
-it = load_dict.get('it', -1)
-metric_val_best = load_dict.get(
-    'loss_val_best', -model_selection_sign * np.inf)
+    epoch_it = load_dict.get('epoch_it', -1)
+    it = load_dict.get('it', -1)
+    metric_val_best = load_dict.get(
+        'loss_val_best', -model_selection_sign * np.inf)
 
-if metric_val_best == np.inf or metric_val_best == -np.inf:
-    metric_val_best = -model_selection_sign * np.inf
+    if metric_val_best == np.inf or metric_val_best == -np.inf:
+        metric_val_best = -model_selection_sign * np.inf
 
-print('Current best validation metric (%s): %.8f'
-      % (model_selection_metric, metric_val_best))
+    print('Current best validation metric (%s): %.8f'
+        % (model_selection_metric, metric_val_best))
 
-scheduler = optim.lr_scheduler.MultiStepLR(
-    optimizer, cfg['training']['scheduler_milestones'],
-    gamma=cfg['training']['scheduler_gamma'], last_epoch=epoch_it)
-logger = SummaryWriter(os.path.join(out_dir, 'logs'))
-# Shorthands
-print_every = cfg['training']['print_every']
-checkpoint_every = cfg['training']['checkpoint_every']
-validate_every = cfg['training']['validate_every']
-visualize_every = cfg['training']['visualize_every']
+    scheduler = optim.lr_scheduler.MultiStepLR(
+        optimizer, cfg['training']['scheduler_milestones'],
+        gamma=cfg['training']['scheduler_gamma'], last_epoch=epoch_it)
+    logger = SummaryWriter(os.path.join(out_dir, 'logs'))
+    # Shorthands
+    print_every = cfg['training']['print_every']
+    checkpoint_every = cfg['training']['checkpoint_every']
+    validate_every = cfg['training']['validate_every']
+    visualize_every = cfg['training']['visualize_every']
 
-# Print model
-nparameters = sum(p.numel() for p in model.parameters())
-logger_py.info(model)
-logger_py.info('Total number of parameters: %d' % nparameters)
-t0b = time.time()
+    # Print model
+    nparameters = sum(p.numel() for p in model.parameters())
+    logger_py.info(model)
+    logger_py.info('Total number of parameters: %d' % nparameters)
+    t0b = time.time()
 
-while True:
-    epoch_it += 1
+    while True:
+        epoch_it += 1
 
-    # for batch in train_loader:
-    for batch in train_loader:
+        # for batch in train_loader:
+        for batch in train_loader:
 
-        it += 1
-        loss = trainer.train_step(batch, it)
-        logger.add_scalar('train/loss', loss, it)
+            it += 1
+            loss = trainer.train_step(batch, it)
+            logger.add_scalar('train/loss', loss, it)
 
-        # Print output
-        if print_every > 0 and (it % print_every) == 0:
-            logger_py.info('[Epoch %02d] it=%03d, loss=%.4f, time=%.4f'
-                           % (epoch_it, it, loss, time.time() - t0b))
-            t0b = time.time()
+            # Print output
+            if print_every > 0 and (it % print_every) == 0:
+                logger_py.info('[Epoch %02d] it=%03d, loss=%.4f, time=%.4f'
+                            % (epoch_it, it, loss, time.time() - t0b))
+                t0b = time.time()
 
-        # Visualize output
-        if visualize_every > 0 and (it % visualize_every) == 0:
-            logger_py.info('Visualizing')
-            trainer.visualize(data_viz, it=it)
+            # Visualize output
+            if visualize_every > 0 and (it % visualize_every) == 0:
+                logger_py.info('Visualizing')
+                trainer.visualize(data_viz, it=it)
 
-        # Save checkpoint
-        if (checkpoint_every > 0 and (it % checkpoint_every) == 0):
-            logger_py.info('Saving checkpoint')
-            print('Saving checkpoint')
-            checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
+            # Save checkpoint
+            if (checkpoint_every > 0 and (it % checkpoint_every) == 0):
+                logger_py.info('Saving checkpoint')
+                print('Saving checkpoint')
+                checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                                loss_val_best=metric_val_best)
 
-        # Backup if necessary
-        if (backup_every > 0 and (it % backup_every) == 0):
-            logger_py.info('Backup checkpoint')
-            checkpoint_io.save('model_%d.pt' % it, epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
+            # Backup if necessary
+            if (backup_every > 0 and (it % backup_every) == 0):
+                logger_py.info('Backup checkpoint')
+                checkpoint_io.save('model_%d.pt' % it, epoch_it=epoch_it, it=it,
+                                loss_val_best=metric_val_best)
 
-        # Run validation
-        if validate_every > 0 and (it % validate_every) == 0:
-            eval_dict = trainer.evaluate(val_loader)
-            metric_val = eval_dict[model_selection_metric]
-            logger_py.info('Validation metric (%s): %.4f'
-                           % (model_selection_metric, metric_val))
+            # Run validation
+            if validate_every > 0 and (it % validate_every) == 0:
+                eval_dict = trainer.evaluate(val_loader)
+                metric_val = eval_dict[model_selection_metric]
+                logger_py.info('Validation metric (%s): %.4f'
+                            % (model_selection_metric, metric_val))
 
-            for k, v in eval_dict.items():
-                logger.add_scalar('val/%s' % k, v, it)
+                for k, v in eval_dict.items():
+                    logger.add_scalar('val/%s' % k, v, it)
 
-            if model_selection_sign * (metric_val - metric_val_best) > 0:
-                metric_val_best = metric_val
-                logger_py.info('New best model (loss %.4f)' % metric_val_best)
-                checkpoint_io.backup_model_best('model_best.pt')
-                checkpoint_io.save('model_best.pt', epoch_it=epoch_it, it=it,
-                                   loss_val_best=metric_val_best)
+                if model_selection_sign * (metric_val - metric_val_best) > 0:
+                    metric_val_best = metric_val
+                    logger_py.info('New best model (loss %.4f)' % metric_val_best)
+                    checkpoint_io.backup_model_best('model_best.pt')
+                    checkpoint_io.save('model_best.pt', epoch_it=epoch_it, it=it,
+                                    loss_val_best=metric_val_best)
 
-        # Exit if necessary
-        if exit_after > 0 and (time.time() - t0) >= exit_after:
-            logger_py.info('Time limit reached. Exiting.')
-            checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
-            exit(3)
+            # Exit if necessary
+            if exit_after > 0 and (time.time() - t0) >= exit_after:
+                logger_py.info('Time limit reached. Exiting.')
+                checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                                loss_val_best=metric_val_best)
+                exit(3)
 
-    # Make scheduler step after full epoch
-    scheduler.step()
+        # Make scheduler step after full epoch
+        scheduler.step()
