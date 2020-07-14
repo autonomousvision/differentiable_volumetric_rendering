bd71c9f4004052d77b3dfb5bbc9d6735f1b8fe9b
Nitish Bhardwaj <nitish.bhardwaj11@gmail.com>
2020-07-14 13:56:37 +0530

    Updated device parameter in loading checkpoint

--- render.py
+++ render.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     model = config.get_model(cfg, device=device, len_dataset=len(dataset))
 
     checkpoint_io = CheckpointIO(out_dir, model=model)
-    checkpoint_io.load(cfg['test']['model_file'])
+    checkpoint_io.load(cfg['test']['model_file'], device=device)
 
     # Generator
     renderer = config.get_renderer(model, cfg, device=device)
